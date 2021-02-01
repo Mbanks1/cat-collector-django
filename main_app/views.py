555cf681d05ae649359409a4c64a1973fbc22900
Mbanks1 <michael1banks@icloud.com>
2021-02-01 12:12:45 -0400

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, redirect
+from .models import Cat, Toy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+# Create your views here.
+
+
+
+# Define the home view
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats })
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form })
+
+def add_feeding(request, cat_id):
+      # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
+
+class CatCreate(CreateView):
+      model = Cat
+      fields = '__all__'
+      success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+      model = Cat
+     # Let's disallow the renaming of a cat by excluding the name field!
+      fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
+# Toy CRUD
+
+class ToyList(ListView):
+      model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
